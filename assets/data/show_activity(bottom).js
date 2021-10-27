f8d2e35fe472554fc378c97f086fb03cecc36a23
LeHaiDangSPKT <88248896+LeHaiDangSPKT@users.noreply.github.com>
2021-10-28 00:14:51 +0700

    Update show_activity(bottom).js

--- assets/data/show_activity(bottom).js
+++ assets/data/show_activity(bottom).js
@@ -4,42 +4,7 @@ function info_activity() {
             img: 'src="../assets/img/hoatdong/1.jpg" alt=""',
             title: 'Chiến dịch xuân tình nguyện Chiến dịch xuân tình nguyện Chiến dịch xuân tình nguyện Chiến dịch xuân tình nguyện',
             sub: 'Đây là hoạt động đầy ý nghĩa của các bạn sinh viên Sư Phạm Kỹ Thuật. Đây là cơ hội để các bạn sinh viên thể hiện sức trẻ, sự nhiệt huyết và tấm lòng của mình.'
-        },
-        {
-            img: 'src="../assets/img/hoatdong/1.jpg" alt=""',
-            title: 'Chiến dịch xuân tình nguyện Chiến dịch xuân tình nguyện Chiến dịch xuân tình nguyện Chiến dịch xuân tình nguyện',
-            sub: 'Đây là hoạt động đầy ý nghĩa của các bạn sinh viên Sư Phạm Kỹ Thuật. Đây là cơ hội để các bạn sinh viên thể hiện sức trẻ, sự nhiệt huyết và tấm lòng của mình.'
-        },
-        {
-            img: 'src="../assets/img/hoatdong/1.jpg" alt=""',
-            title: 'Chiến dịch xuân tình nguyện Chiến dịch xuân tình nguyện Chiến dịch xuân tình nguyện Chiến dịch xuân tình nguyện',
-            sub: 'Đây là hoạt động đầy ý nghĩa của các bạn sinh viên Sư Phạm Kỹ Thuật. Đây là cơ hội để các bạn sinh viên thể hiện sức trẻ, sự nhiệt huyết và tấm lòng của mình.'
-        },
-        {
-            img: 'src="../assets/img/hoatdong/1.jpg" alt=""',
-            title: 'Chiến dịch xuân tình nguyện Chiến dịch xuân tình nguyện Chiến dịch xuân tình nguyện Chiến dịch xuân tình nguyện',
-            sub: 'Đây là hoạt động đầy ý nghĩa của các bạn sinh viên Sư Phạm Kỹ Thuật. Đây là cơ hội để các bạn sinh viên thể hiện sức trẻ, sự nhiệt huyết và tấm lòng của mình.'
-        },
-        {
-            img: 'src="../assets/img/hoatdong/1.jpg" alt=""',
-            title: 'Chiến dịch xuân tình nguyện Chiến dịch xuân tình nguyện Chiến dịch xuân tình nguyện Chiến dịch xuân tình nguyện',
-            sub: 'Đây là hoạt động đầy ý nghĩa của các bạn sinh viên Sư Phạm Kỹ Thuật. Đây là cơ hội để các bạn sinh viên thể hiện sức trẻ, sự nhiệt huyết và tấm lòng của mình.'
-        },
-        {
-            img: 'src="../assets/img/hoatdong/1.jpg" alt=""',
-            title: 'Chiến dịch xuân tình nguyện Chiến dịch xuân tình nguyện Chiến dịch xuân tình nguyện Chiến dịch xuân tình nguyện',
-            sub: 'Đây là hoạt động đầy ý nghĩa của các bạn sinh viên Sư Phạm Kỹ Thuật. Đây là cơ hội để các bạn sinh viên thể hiện sức trẻ, sự nhiệt huyết và tấm lòng của mình.'
-        },
-        {
-            img: 'src="../assets/img/hoatdong/1.jpg" alt=""',
-            title: 'Chiến dịch xuân tình nguyện Chiến dịch xuân tình nguyện Chiến dịch xuân tình nguyện Chiến dịch xuân tình nguyện',
-            sub: 'Đây là hoạt động đầy ý nghĩa của các bạn sinh viên Sư Phạm Kỹ Thuật. Đây là cơ hội để các bạn sinh viên thể hiện sức trẻ, sự nhiệt huyết và tấm lòng của mình.'
-        },
-        {
-            img: 'src="../assets/img/hoatdong/1.jpg" alt=""',
-            title: 'Chiến dịch xuân tình nguyện Chiến dịch xuân tình nguyện Chiến dịch xuân tình nguyện Chiến dịch xuân tình nguyện',
-            sub: 'Đây là hoạt động đầy ý nghĩa của các bạn sinh viên Sư Phạm Kỹ Thuật. Đây là cơ hội để các bạn sinh viên thể hiện sức trẻ, sự nhiệt huyết và tấm lòng của mình.'
-        },
+        }
     ]
 }
 export default info_activity;
